142a9512163208ad87c6a49e317ecf26161a5be5
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-05 20:01:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,23 @@
 //your code here
+//your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+touristSpots.sort((a,b)=>{
+    let nameA = a.toLowerCase().substring(2); // ignorer upperCase
+    let nameB = b.toLowerCase().substring(2); // ignorer upperCase
+    if (nameA < nameB) {
+        return -1;
+    }
+    if (nameA > nameB) {
+    return 1;
+    }
 
-const bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Who', 'Rolling Stones'];
-
-function sortBandNames(names) {
-  const articles = ['the', 'an', 'a'];
-
-  // Strip articles from band names
-  const strippedNames = names.map(name => {
-    const words = name.split(' ');
-    return words.filter(word => !articles.includes(word.toLowerCase())).join(' ');
-  });
-
-  // Sort the stripped names in lexicographic order
-  const sortedNames = strippedNames.sort((a, b) => a.localeCompare(b));
-
-  // Create a list of li elements for each band name
-  const listItems = sortedNames.map(name => {
-    const li = document.createElement('li');
-    li.textContent = name;
-    return li;
-  });
-
-  // Add the list items to the unordered list with the id "band"
-  const bandList = document.querySelector('#band');
-  listItems.forEach(li => bandList.appendChild(li));
-}
-
-sortBandNames(bandNames);
+    // names must be equal
+    return 0;
+})
+// Generate the list items
+const ul = document.getElementById('bands');
+touristSpots.forEach((bandName) => {
+  const li = document.createElement('li');
+  li.textContent = bandName;
+  ul.appendChild(li);
+});
